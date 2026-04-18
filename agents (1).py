8c46089a26614aa9d5b88afc24a718f691fc7140
YEL56 <leey2@uchicago.edu>
2026-04-18 13:11:16 -0500

    Update agents (1).py

--- agents (1).py
+++ agents (1).py
@@ -46,10 +46,10 @@ class SchellingAgent(CellAgent):
         if self.random.random() < self.model.intervention_prob: ## generate a random number btw 0 and 1. If that's smaller than the intervention probability,intervention happens 
    
             partner = self.random.choice(unlike_neighbors)      ## now than an agent received intervention, then, pick one unlike neighbor at random
-            self.homophily = max(self.model.homophily_floor, self.homophily -self.model.intervention_effect) #don't let homophily go below minimum 
-            partner.homophily = max(self.model.homophily_floor, partner.homophily - self.model.intervention_effect)
-            self.received_intervention_this_round = True
-            partner.received_intervention_this_round = True
+            self.homophily = max(self.model.homophily_floor, self.homophily -self.model.intervention_effect) ## don't let homophily go below minimum "homophily_floor"
+            partner.homophily = max(self.model.homophily_floor, partner.homophily - self.model.intervention_effect) ## don't let homophily go below minimum "homophily_floor" for counterpart agent (neighbor) as well
+            self.received_intervention_this_round = True     ## both me and counterpart agent (neighbor) I interacted with received an intervention
+            partner.received_intervention_this_round = True  ## both me and counterpart agent (neighbor) I interacted with received an intervention
 
 #move if unhappy
     def step(self): 
@@ -58,27 +58,7 @@ class SchellingAgent(CellAgent):
             self.cell = self.model.grid.select_random_empty_cell()
 
 
-##LIST OF STUFF TO DO FOR MODEL 
-#1.MAKE SURE MODEL RESETS SELF.HAPPY = 0 AT THE START 
-#2.MAKE self.intervention_effect = scenario.intervention_effect AND!!! self.homophily_floor = scenario.homophily_floor ---> effect 0.2
-#2-2. homophily drop by 0.04 
-#3. self.intervention_effect = scenario.intervention_effect AND self.homophily_floor = scenario.homophily_floor ---> homophily floor 0.1
-#4. self.model.intervention_effect  % 정하기 
 
-#5. 이것도 하기: self.happy = 0 
-# self.agents.shuffle_do("step")        # move first
-# self.agents.shuffle_do("cooperate")   # then intervene
-# self.agents.do("assign_state")        # then recompute happiness using new homophily
-# self.datacollector.collect(self)
-# self.running = self.happy < len(self.agents)
-
-# this sequences the agents to move after intervention. In the AGENTS section only this: 
-#def step(self):
-    #if not self.happy:
-        # self.cell = self.model.grid.select_random_empty_cell()
-
-#6. add new intervention stage in step()
-#7. place intervention after movement! after assign_state????... so after self.agents.do("assign_state") and before self.data.collect(self)????
 
 
 
